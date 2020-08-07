8c55432af4fdc3f4db30684ec410b7dc6b79577a
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-08-07 14:28:56 +0900

    templated conversion to/from OpenCV types

--- include/object_detection_msgs/cv_conversions.hpp
+++ include/object_detection_msgs/cv_conversions.hpp
@@ -13,49 +13,94 @@
 
 namespace object_detection_msgs {
 
-static inline cv::Point toCvPoint(const Point &point_msg) {
-  return cv::Point(point_msg.x, point_msg.y);
+// **************************
+// Conversion to OpenCV types
+// **************************
+
+template < typename T > static inline cv::Point_< T > toCvPoint_(const Point &point_msg) {
+  return cv::Point_< T >(static_cast< T >(point_msg.x), static_cast< T >(point_msg.y));
 }
 
-static inline std::vector< cv::Point > toCvPoints(const Points &points_msg) {
-  std::vector< cv::Point > cv_points;
+template < typename T >
+static inline std::vector< cv::Point_< T > > toCvPoints_(const Points &points_msg) {
+  std::vector< cv::Point_< T > > cv_points;
   BOOST_FOREACH (const Point &point_msg, points_msg.points) {
-    cv_points.push_back(toCvPoint(point_msg));
+    cv_points.push_back(toCvPoint_< T >(point_msg));
   }
   return cv_points;
 }
 
-static inline std::vector< std::vector< cv::Point > >
-toCvContours(const std::vector< Points > &contours_msg) {
-  std::vector< std::vector< cv::Point > > cv_contours;
+template < typename T >
+static inline std::vector< std::vector< cv::Point_< T > > >
+toCvContours_(const std::vector< Points > &contours_msg) {
+  std::vector< std::vector< cv::Point_< T > > > cv_contours;
   BOOST_FOREACH (const Points &points_msg, contours_msg) {
-    cv_contours.push_back(toCvPoints(points_msg));
+    cv_contours.push_back(toCvPoints_< T >(points_msg));
   }
   return cv_contours;
 }
 
-static inline Point toPointMsg(const int x, const int y) {
+// ********************************************************
+// Conversions to cv::Point and cv::Point2f for convenience
+// ********************************************************
+
+// cv::Point
+
+static inline cv::Point toCvPoint(const Point &point_msg) {
+  return toCvPoint_< cv::Point::value_type >(point_msg);
+}
+
+static inline std::vector< cv::Point > toCvPoints(const Points &points_msg) {
+  return toCvPoints_< cv::Point::value_type >(points_msg);
+}
+
+static inline std::vector< std::vector< cv::Point > >
+toCvContours(const std::vector< Points > &contours_msg) {
+  return toCvContours_< cv::Point::value_type >(contours_msg);
+}
+
+// cv::Point2f
+
+static inline cv::Point2f toCvPoint2f(const Point &point_msg) {
+  return toCvPoint_< cv::Point2f::value_type >(point_msg);
+}
+
+static inline std::vector< cv::Point2f > toCvPoints2f(const Points &points_msg) {
+  return toCvPoints_< cv::Point2f::value_type >(points_msg);
+}
+
+static inline std::vector< std::vector< cv::Point2f > >
+toCvContours2f(const std::vector< Points > &contours_msg) {
+  return toCvContours_< cv::Point2f::value_type >(contours_msg);
+}
+
+// *******************************
+// Conversion to ROS message types
+// *******************************
+
+template < typename T > static inline Point toPointMsg(const T x, const T y) {
   Point point_msg;
-  point_msg.x = x;
-  point_msg.y = y;
+  point_msg.x = static_cast< int >(x);
+  point_msg.y = static_cast< int >(y);
   return point_msg;
 }
 
-static inline Point toPointMsg(const cv::Point &cv_point) {
+template < typename T > static inline Point toPointMsg(const cv::Point_< T > &cv_point) {
   return toPointMsg(cv_point.x, cv_point.y);
 }
 
-static inline Points toPointsMsg(const std::vector< cv::Point > &cv_points) {
+template < typename T >
+static inline Points toPointsMsg(const std::vector< cv::Point_< T > > &cv_points) {
   Points points_msg;
-  BOOST_FOREACH (const cv::Point &cv_point, cv_points) {
+  BOOST_FOREACH (const cv::Point_< T > &cv_point, cv_points) {
     points_msg.points.push_back(toPointMsg(cv_point));
   }
   return points_msg;
 }
 
-static inline Points toPointsMsg(const cv::Rect &cv_rect) {
+template < typename T > static inline Points toPointsMsg(const cv::Rect_< T > &cv_rect) {
   Points points_msg;
-  const cv::Point tl(cv_rect.tl()), br(cv_rect.br());
+  const cv::Point_< T > tl(cv_rect.tl()), br(cv_rect.br());
   points_msg.points.push_back(toPointMsg(tl.x, tl.y));
   points_msg.points.push_back(toPointMsg(br.x, tl.y));
   points_msg.points.push_back(toPointMsg(br.x, br.y));
@@ -63,14 +108,16 @@ static inline Points toPointsMsg(const cv::Rect &cv_rect) {
   return points_msg;
 }
 
-static inline Points toPointsMsg(const cv::Size &cv_size) {
-  return toPointsMsg(cv::Rect(cv::Point(0, 0), cv_size));
+template < typename T > static inline Points toPointsMsg(const cv::Size_< T > &cv_size) {
+  return toPointsMsg(
+      cv::Rect_< T >(cv::Point_< T >(static_cast< T >(0), static_cast< T >(0)), cv_size));
 }
 
+template < typename T >
 static inline std::vector< Points >
-toContoursMsg(const std::vector< std::vector< cv::Point > > &cv_contours) {
+toContoursMsg(const std::vector< std::vector< cv::Point_< T > > > &cv_contours) {
   std::vector< Points > contours_msg;
-  BOOST_FOREACH (const std::vector< cv::Point > &cv_points, cv_contours) {
+  BOOST_FOREACH (const std::vector< cv::Point_< T > > &cv_points, cv_contours) {
     contours_msg.push_back(toPointsMsg(cv_points));
   }
   return contours_msg;
