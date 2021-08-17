5db042eb5d1c4ad1ee9978355631f068519fe15b
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2021-08-17 15:04:04 +0000

    organize .cpp files

--- src/non_maximum_suppressor.cpp
+++
@@ -1,5 +0,0 @@
-#include <nodelet/nodelet.h>
-#include <object_detection_msgs/non_maximum_suppressor.hpp>
-#include <pluginlib/class_list_macros.h>
-
-PLUGINLIB_EXPORT_CLASS(object_detection_msgs::NonMaximumSuppressor, nodelet::Nodelet);
--- src/object_detection_msg_tools.cpp
+++ src/object_detection_msg_tools.cpp
@@ -1,5 +1,7 @@
 #include <nodelet/nodelet.h>
+#include <object_detection_msgs/non_maximum_suppressor.hpp>
 #include <object_detection_msgs/synchronized_object_publisher.hpp>
 #include <pluginlib/class_list_macros.h>
 
+PLUGINLIB_EXPORT_CLASS(object_detection_msgs::NonMaximumSuppressor, nodelet::Nodelet);
 PLUGINLIB_EXPORT_CLASS(object_detection_msgs::SynchronizedObjectPublisher, nodelet::Nodelet);
